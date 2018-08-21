6feb08197d4042402c8b9e641bbdc32eb221cf6c
marchandau <aurelie.mrchd@gmail.com>
2018-08-21 10:52:39 +0200

    optimisation requetes bdd

--- src/main/java/com/apiwatch/repositories/ApiaryRepository.java
+++ src/main/java/com/apiwatch/repositories/ApiaryRepository.java
@@ -1,15 +1,20 @@
 package com.apiwatch.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.apiwatch.entities.Apiary;
 
-
 @Service
 @Repository
 public interface ApiaryRepository  extends MongoRepository<Apiary ,String>{
-
+	
+	List<Apiary> findApiaryByUsername(String username);
+	
+	Apiary findApiaryById(String id);
+	
 }
 
