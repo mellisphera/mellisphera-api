9a12e91194c98ad5890999af551c599c515e83f3
marchandau <aurelie.mrchd@gmail.com>
2018-08-27 09:52:29 +0200

    modifier/supprimer observations ruche/rucher

--- src/main/java/com/apiwatch/controllers/ProcessReportController.java
+++ src/main/java/com/apiwatch/controllers/ProcessReportController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apiwatch.entities.Apiary;
 import com.apiwatch.entities.ProcessReport;
 import com.apiwatch.repositories.ProcessReportRepository;
 
@@ -62,6 +64,27 @@ public class ProcessReportController {
         Collections.reverse(reports);
         return reports;
     }
+    
+    @DeleteMapping("/{id}")
+	 public void delete(@PathVariable("id") String id){
+	    this.processReportRepository.deleteById(id);
+	 }
+    
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT) 
+    public void update(@PathVariable("id") String id, @RequestBody ProcessReport processReport){   
     	
+    	ProcessReport report= this.processReportRepository.findProcessReportById(id);
+    	
+    	report.setDate(processReport.getDate());
+    	report.setType(processReport.getType());
+    	report.setSentence(processReport.getSentence());
+    	report.setId(processReport.getId());
+    	report.setIdApiary(processReport.getIdApiary());
+    	report.setIdHive(processReport.getIdHive());
+    	report.setNluScore(processReport.getNluScore());
+    	
+ 		this.processReportRepository.save(report);
+    }
+   
 	
 }
