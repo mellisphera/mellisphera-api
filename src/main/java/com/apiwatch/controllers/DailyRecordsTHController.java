5942018cf647ffc0fec31050f44259aa11061f4d
pezzoni.mickael@gmail.com <pezzoni.mickael@gmail.com>
2018-08-29 10:34:44 +0200

    Ajout requete DailyRecordsTH dernière date

--- src/main/java/com/apiwatch/controllers/DailyRecordsTHController.java
+++ src/main/java/com/apiwatch/controllers/DailyRecordsTHController.java
@@ -1,6 +1,7 @@
 package com.apiwatch.controllers;
 
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +30,7 @@ public class DailyRecordsTHController {
 		return this.dailyRecordsTHRepository.findAll();
 	}
 	
+
 	@RequestMapping(value="/hive/{idHive}", method = RequestMethod.GET, produces={"application/json"})
 	public List<DailyRecordsTH> getByIdHive(@PathVariable("idHive") String idHive){
 		return this.dailyRecordsTHRepository.findDailyRecordsTHByIdHive(idHive);
@@ -38,8 +40,22 @@ public class DailyRecordsTHController {
 	public void deleteById(@PathVariable("id") String id) {
 		this.dailyRecordsTHRepository.deleteById(id);
 	}
+	/*
+	@RequestMapping(value="/last/{idHive}", method = RequestMethod.GET, produces={"application/json"})
+	public List<DailyRecordsTH> getByLastDate(@PathVariable("idHive") String idHive) {
+		List<DailyRecordsTH> dailyRecords = this.dailyRecordsTHRepository.findDailyRecordsTHByrecordDate(new Date());
+		return dailyRecords;
+	}*/
 	
-	
-	
+	@RequestMapping(value="/last/{idHive}", method = RequestMethod.GET, produces={"application/json"})
+	public DailyRecordsTH getByLastDate(@PathVariable("idHive") String idHive) {
+		List<DailyRecordsTH> dailyRecTh = this.dailyRecordsTHRepository.findDailyRecordsTHByIdHive(idHive);
+		for(DailyRecordsTH d : dailyRecTh) {
+			if(new Date().getDate() == d.getRecordDate().getDate()) {
+				return d;
+			}
+		}
+		return null;
+	}
 	
 }
