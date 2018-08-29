7543d01a96a86ae63eb0fa1038f95ab132514d51
pezzoni.mickael@gmail.com <pezzoni.mickael@gmail.com>
2018-08-29 08:58:11 +0200

    Ajout requete DailyRecordsW

---
+++ src/main/java/com/apiwatch/controllers/DailyRecordsWController.java
@@ -0,0 +1,45 @@
+package com.apiwatch.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apiwatch.entities.DailyRecordsTH;
+import com.apiwatch.entities.DailyRecordsW;
+import com.apiwatch.repositories.DailyRecordsWRepository;
+
+@Service
+@RestController
+@RequestMapping("/dailyRecordsW")
+@CrossOrigin(origins = {"http://localhost:4200", "http://***REMOVED***:4200","http://***REMOVED***:4300"})
+public class DailyRecordsWController {
+
+	@Autowired
+	private DailyRecordsWRepository dailyRecordsWRepository;
+	
+	public DailyRecordsWController(DailyRecordsWRepository dailyRecordsWRepository) {
+		this.dailyRecordsWRepository = dailyRecordsWRepository;
+	}
+	
+	@RequestMapping(value = "/all", method = RequestMethod.GET, produces={"application/json"})
+	public List<DailyRecordsW> getAll(){
+		return this.dailyRecordsWRepository.findAll();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET, produces={"application/json"})
+	public Optional<DailyRecordsW> getById(@PathVariable("id") String id){
+		return this.dailyRecordsWRepository.findById(id);
+	}
+	@RequestMapping(value="/hive/{idHive}", method = RequestMethod.GET, produces={"application/json"})
+	public List<DailyRecordsW>getByidHive(@PathVariable("idHive") String idHive){
+		return this.dailyRecordsWRepository.findDailyRecordsWById(idHive);
+	}
+
+}
