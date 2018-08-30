6d106d2c0d427a0e333a99a3de73020ea365a464
marchandau <aurelie.mrchd@gmail.com>
2018-08-30 17:18:01 +0200

    rapport ok

--- src/main/java/com/apiwatch/controllers/ProcessReportTempController.java
+++ src/main/java/com/apiwatch/controllers/ProcessReportTempController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apiwatch.entities.ProcessReport;
 import com.apiwatch.entities.ProcessReportTemp;
+import com.apiwatch.repositories.ProcessReportRepository;
 import com.apiwatch.repositories.ProcessReportTempRepository;
 
 @Service
@@ -22,19 +25,45 @@ public class ProcessReportTempController {
 	
 	@Autowired
     private ProcessReportTempRepository processReportTempRepository;
+	@Autowired
+    private ProcessReportRepository processReportRepository;
 	
     public ProcessReportTempController() {
 	    }
+ 
+    
     
     public ProcessReportTempController(ProcessReportTempRepository ProcessReportTempRepository) {
         this.processReportTempRepository = ProcessReportTempRepository;
     }
-
+    
     @RequestMapping(value = "/{username}", method = RequestMethod.GET, produces={"application/json"})
-    public List<ProcessReportTemp> getReportsApiray(@PathVariable("username") String username){
+    public List<ProcessReportTemp> getReports(@PathVariable("username") String username){
         List<ProcessReportTemp> reports = this.processReportTempRepository.findProcessReportTempByUsername(username);
         Collections.reverse(reports);
         return reports;
     }
 
+    @DeleteMapping("/delete/{id}")
+	 public void delete(@PathVariable("id") String id){
+	    this.processReportTempRepository.deleteById(id);
+	 }
+    
+    @RequestMapping(value = "/add/{username}", method = RequestMethod.GET, produces={"application/json"})
+    public void addReports(@PathVariable("username") String username){
+        List<ProcessReportTemp> reports = this.processReportTempRepository.findProcessReportTempByUsername(username);
+        for(ProcessReportTemp temp : reports) {
+        	ProcessReport process = new ProcessReport();
+        	process.setDate(temp.getDate());
+        	process.setIdApiary(temp.getIdApiary());
+        	process.setIdHive(temp.getIdHive());
+        	process.setIdLHive(temp.getIdLHive());
+        	process.setNluScore(temp.getNluScore());
+        	process.setSentence(temp.getSentence());
+        	process.setUsername(temp.getUsername());
+        	process.setType(temp.getType());
+        	this.processReportRepository.insert(process);
+        }
+    }
+    
 }
