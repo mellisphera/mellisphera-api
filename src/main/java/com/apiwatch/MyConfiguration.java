b1b57a2bfcd1f0e3f35c35320b60459458abfc09
mickael2407 <pezzoni.mickael@gmail.com>
2018-10-11 21:19:45 +0200

    ajout requete dailyStockHoney

--- src/main/java/com/apiwatch/MyConfiguration.java
+++ src/main/java/com/apiwatch/MyConfiguration.java
@@ -20,7 +20,7 @@ public class MyConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","http://localhost:4300","http://51.68.71.91","http://51.68.71.91:4200")
+                        .allowedOrigins("http://localhost:4200","http://localhost:4300","http://51.68.71.91","http://51.68.71.91:4200","http://192.168.0.108:4200")
                         .allowedMethods("PUT", "DELETE","POST","GET");
             }
         };
