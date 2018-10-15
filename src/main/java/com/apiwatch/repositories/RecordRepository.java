dbae57da91434249e0d973a4a8b330054f640b4e
mickael2407 <pezzoni.mickael@gmail.com>
2018-10-15 13:19:23 +0200

    update

--- src/main/java/com/apiwatch/repositories/RecordRepository.java
+++ src/main/java/com/apiwatch/repositories/RecordRepository.java
@@ -3,7 +3,10 @@ package com.apiwatch.repositories;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.apiwatch.entities.Record;
+import java.util.List;
 
 public interface RecordRepository  extends MongoRepository<Record,String> {
+    
+    public List<Record> findRecordByIdHive(String idHive);
 
 }
