d2c663dd15c5c203f1c51897b4571378cd827eee
mickael <pezzoni.mickael@gmail.com>
2018-10-28 14:44:04 +0100

    update

--- src/main/java/com/apiwatch/MyConfiguration.java
+++ src/main/java/com/apiwatch/MyConfiguration.java
@@ -20,7 +20,7 @@ public class MyConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(System.getenv("URL_ALLOW"))
+                        .allowedOrigins("http://localhost:4200","http://localhost:4300","http://51.68.71.91","http://51.68.71.91:4200","http://51.68.71.91:4300","http://192.168.0.108:4200", "http://localhost")
                         .allowedMethods("PUT", "DELETE","POST","GET");
             }
         };
