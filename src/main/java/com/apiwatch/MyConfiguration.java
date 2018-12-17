831e4187538a320768da2e83df6499bfade37774
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2018-12-17 11:47:34 +0100

    modification ssl

--- src/main/java/com/apiwatch/MyConfiguration.java
+++ src/main/java/com/apiwatch/MyConfiguration.java
@@ -20,7 +20,7 @@ public class MyConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","http://localhost:4300","http://51.68.71.91","https://51.68.71.91","http://51.68.71.91:4200","http://51.68.71.91:4300","http://192.168.0.108:4200", "http://localhost")
+                        .allowedOrigins("http://localhost:4200","http://localhost:4300","http://51.68.71.91","https://app.mellisphera.com","https://51.68.71.91","http://51.68.71.91:4200","http://51.68.71.91:4300","http://192.168.0.108:4200", "http://localhost")
                         .allowedMethods("PUT", "DELETE","POST","GET");
             }
         };
