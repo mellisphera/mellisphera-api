ced28fddfaa2165cd2306cb813f728bf6cbe7734
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2018-12-18 11:26:56 +0100

    essai ajout log connection

--- src/main/java/com/apiwatch/MyConfiguration.java
+++ src/main/java/com/apiwatch/MyConfiguration.java
@@ -12,11 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MyConfiguration {
-    
+public class MyConfiguration implements WebMvcConfigurer{
+ 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer () {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
