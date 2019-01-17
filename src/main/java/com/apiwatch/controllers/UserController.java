b7de46c8f04b66fad2a0a95c53c6df29aebadedb
Mickael Pezzoni <pezzoni.mickael@gmail.com>
2019-01-17 13:55:39 +0100

    modification properties

--- src/main/java/com/apiwatch/controllers/UserController.java
+++ src/main/java/com/apiwatch/controllers/UserController.java
@@ -44,40 +44,40 @@ import org.json.*;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired private UserRepository userRepository;
 	@Autowired private ConnectionRepository connectionRepository;
-	
-    public UserController() {
-    	
-	    }
 
-    public UserController(UserRepository userRepository, ConnectionRepository connectionRepository) {
-	        this.userRepository = userRepository;
-	        //this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
+	public UserController() {
 
-    @PostMapping("/signup")
-    public void signUp(@RequestBody User u){
-    	Date date = new Date();
-        // display time and date using toString()
-    	u.setCreatedAt(date);
-    	this.userRepository.insert(u);
-    }
-    
-    @GetMapping("/all")
-    public List<User> getAll(){
-    List<User> Users=this.userRepository.findAll();
-    return Users;
-    }
-    
-    public void setDataConnection(User user, HttpServletRequest request){
-    	HttpsGetRequest http = new HttpsGetRequest();
-    	Location location = null;
-    	http.setIp(request.getRemoteAddr());
-    	System.err.println(request.getRemoteAddr());
-    	JSONParser parser = new JSONParser(); 
-    	try {
+	}
+
+	public UserController(UserRepository userRepository, ConnectionRepository connectionRepository) {
+		this.userRepository = userRepository;
+		//this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	@PostMapping("/signup")
+	public void signUp(@RequestBody User u){
+		Date date = new Date();
+		// display time and date using toString()
+		u.setCreatedAt(date);
+		this.userRepository.insert(u);
+	}
+
+	@GetMapping("/all")
+	public List<User> getAll(){
+		List<User> Users=this.userRepository.findAll();
+		return Users;
+	}
+
+	public void setDataConnection(User user, HttpServletRequest request){
+		HttpsGetRequest http = new HttpsGetRequest();
+		Location location = null;
+		http.setIp(request.getRemoteAddr());
+		System.err.println(request.getRemoteAddr());
+		JSONParser parser = new JSONParser(); 
+		try {
 			JSONObject json = (JSONObject) parser.parse(http.getLocation());
 			location = new Location(json);
 			System.out.println(location.toString());
@@ -86,57 +86,57 @@ public class UserController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        long nbConnectionTotal = user.getConnexions();
-        Connection newConnection = new Connection(new Date(),request.getRemoteAddr(),user.getId(),user.getLogin(),location);
-        user.setConnexions(nbConnectionTotal+1);
-        user.setLastConnection(new Date());
-        this.userRepository.save(user);
-        this.connectionRepository.save(newConnection);
-    }
-    
-    @GetMapping("/usernames")
-    public List<String> getAllRuchers(){
-    List<User> Users=this.userRepository.findAll();
-    List<String> names = new ArrayList<>();
-    for(User e : Users) {
-    	names.add(e.getLogin().getUsername());
-    }
-    return names;
-    }
-   
-    @GetMapping("/createdAt")
-    public List<User> findByCreatedDate(){
-    
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); // Quoted "Z" to indicate UTC, no timezone offset
-	    List<User> Users=this.userRepository.findAll();
-	    List<User> todayUsers = new ArrayList<>();
-	    //System.out.println("Today date : "+ nowAsISO );
-	    
-	    System.out.println("new Date().getDate(): " + df.format(new Date()));
-    	for(User e : Users) {
-    		//System.out.println("e.getCreatedAt.getDate(): " + df.format(e.getCreatedAt()));
-    		//System.out.println("USERNAME : " + e.getUsername());
-    		if(df.format(e.getCreatedAt()).equals(df.format(new Date()))) {
-    			todayUsers.add(e);
-    		}
-    		
-    	 }
-    	    
-   	 
-    	return todayUsers;
-    }
-    
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") String id){
-      this.userRepository.deleteById(id);
-    }
-    
-    @GetMapping("/{id}")
-    public Optional<User> getById(@PathVariable("id") String id){
-        Optional<User> User = this.userRepository.findById(id);
-        return User;
-    }
-    /*@RequestMapping(value="/loguser", method=RequestMethod.POST,consumes="application/json", produces = "application/json")
+		long nbConnectionTotal = user.getConnexions();
+		Connection newConnection = new Connection(new Date(),request.getRemoteAddr(),user.getId(),user.getLogin(),location);
+		user.setConnexions(nbConnectionTotal+1);
+		user.setLastConnection(new Date());
+		this.userRepository.save(user);
+		this.connectionRepository.save(newConnection);
+	}
+
+	@GetMapping("/usernames")
+	public List<String> getAllRuchers(){
+		List<User> Users=this.userRepository.findAll();
+		List<String> names = new ArrayList<>();
+		for(User e : Users) {
+			names.add(e.getLogin().getUsername());
+		}
+		return names;
+	}
+
+	@GetMapping("/createdAt")
+	public List<User> findByCreatedDate(){
+
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd"); // Quoted "Z" to indicate UTC, no timezone offset
+		List<User> Users=this.userRepository.findAll();
+		List<User> todayUsers = new ArrayList<>();
+		//System.out.println("Today date : "+ nowAsISO );
+
+		System.out.println("new Date().getDate(): " + df.format(new Date()));
+		for(User e : Users) {
+			//System.out.println("e.getCreatedAt.getDate(): " + df.format(e.getCreatedAt()));
+			//System.out.println("USERNAME : " + e.getUsername());
+			if(df.format(e.getCreatedAt()).equals(df.format(new Date()))) {
+				todayUsers.add(e);
+			}
+
+		}
+
+
+		return todayUsers;
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") String id){
+		this.userRepository.deleteById(id);
+	}
+
+	@GetMapping("/{id}")
+	public Optional<User> getById(@PathVariable("id") String id){
+		Optional<User> User = this.userRepository.findById(id);
+		return User;
+	}
+	/*@RequestMapping(value="/loguser", method=RequestMethod.POST,consumes="application/json", produces = "application/json")
     public Boolean checkLogin(@RequestBody Login login ){
         User user = null;
         user = this.userRepository.findUserBylogin(login);  
@@ -148,22 +148,22 @@ public class UserController {
         }
     }*/
 
-    @RequestMapping(value="/loguser", method=RequestMethod.POST,consumes="application/json", produces = "application/json")
-    public User checkLogin(@RequestBody Login login, HttpServletRequest request, HttpServletResponse res){
-        System.out.println(login.toString());
-        List<User> user = this.userRepository.findAll();
-        for(User u : user){
-            if(u.getLogin().getUsername().equals(login.getUsername()) && u.getLogin().getPassword().equals(login.getPassword())){
-            	System.out.println(request.getRemoteAddr());
-            	if(request.getRemoteAddr() != "0:0:0:0:0:0:0:1"){
-            			 this.setDataConnection(u,request);
-            	}
-                //resultatLogin.put(u.getLastConnection(), true);
-                return u;
-            }
-        }
-        //resultatLogin.put(new Date(), false);
-        return new User(true);
-    }
-	
+	@RequestMapping(value="/loguser", method=RequestMethod.POST,consumes="application/json", produces = "application/json")
+	public User checkLogin(@RequestBody Login login, HttpServletRequest request, HttpServletResponse res){
+		System.out.println(login.toString());
+		List<User> user = this.userRepository.findAll();
+		for(User u : user){
+			if(u.getLogin().getUsername().equals(login.getUsername()) && u.getLogin().getPassword().equals(login.getPassword())){
+				System.out.println(request.getRemoteAddr());
+				if(request.getRemoteAddr() != "0:0:0:0:0:0:0:1"){
+					this.setDataConnection(u,request);
+				}
+				//resultatLogin.put(u.getLastConnection(), true);
+				return u;
+			}
+		}
+		//resultatLogin.put(new Date(), false);
+		return new User(true);
+	}
+
 }
