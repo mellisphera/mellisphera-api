2f79333771e53721fb54ae65a85d703229eea3ab
Eric Papet <e.papet@dev1-0.com>
2019-01-28 11:39:37 +0100

    First commit with all new class

---
+++ src/main/java/com/apiwatch/controllers/AuthRestApiController.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package com.apiwatch.controllers;
+
+import java.util.GregorianCalendar;
+
+import javax.validation.Valid;
+
+import com.apiwatch.entities.Login;
+import com.apiwatch.entities.User;
+import com.apiwatch.repositories.UserRepository;
+import com.apiwatch.security.entities.ApiWatchUserDetails;
+import com.apiwatch.security.jwt.JwtProvider;
+import com.apiwatch.security.message.request.LoginForm;
+import com.apiwatch.security.message.response.JwtResponse;
+import com.apiwatch.security.message.response.ResponseMessage;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author epa
+ *
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestApiController {
+	Log log = LogFactory.getLog(AuthRestApiController.class);
+
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtProvider jwtProvider;
+
+	/**
+	 * 
+	 * @param loginRequest
+	 * @return
+	 */
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
+		log.info(" Sign Up : username :" + loginRequest.getUsername() + " password:" + loginRequest.getPassword());
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
+		log.info(" Sign Up : AUTH OK");
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		String jwt = jwtProvider.generateJwtToken(authentication);
+		ApiWatchUserDetails apiWatchUserDetails = (ApiWatchUserDetails) authentication.getPrincipal();
+
+		return ResponseEntity.ok(new JwtResponse(jwt, apiWatchUserDetails.getUsername(), apiWatchUserDetails.getAuthorities()));
+	}
+
+	/**
+	 * 	
+	 * @param signUpRequest
+	 * @return
+	 */
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody User signUpRequest) {
+		log.info(" Sign Up : username :"+ signUpRequest.getLogin().getUsername() +" password :"+signUpRequest.getPassword()+" email:" + signUpRequest.getEmail());
+		//
+		Login credential = new Login(signUpRequest.getUsername(),encoder.encode(signUpRequest.getPassword()));
+		//
+		log.info(" Sign Up : Login :"+ credential.getUsername() +" password :" + credential.getPassword());
+		//Search if user already exit
+		if (userRepository.findUserByLogin(credential) != null) {
+			log.error(" Sign Up : Fail -> Username is already taken! "+ signUpRequest.getLogin().getUsername() );
+			return new ResponseEntity<>(new ResponseMessage("Fail -> Username is already taken!"),
+					HttpStatus.BAD_REQUEST);
+		}
+		/**
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return new ResponseEntity<>(new ResponseMessage("Fail -> Username is already taken!"),
+					HttpStatus.BAD_REQUEST);
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return new ResponseEntity<>(new ResponseMessage("Fail -> Email is already in use!"),
+					HttpStatus.BAD_REQUEST);
+		}
+	**/
+		//public User(String id, Date createdAt, Login login, String phone, String email, long connexions,Date lastConnection, String fullName, String position, String city, int levelUser, String country)
+		// Creating user's account
+		User user = new User(signUpRequest.getId(), GregorianCalendar.getInstance().getTime(),credential,signUpRequest.getPhone(),signUpRequest.getEmail(),0,null,signUpRequest.getFullName(),signUpRequest.getPosition(),signUpRequest.getCity(), signUpRequest.getCountry(),signUpRequest.getRoles());
+		user.setUsername(credential.getUsername());
+		user.setPassword(credential.getPassword());
+		userRepository.insert(user);
+		
+		return new ResponseEntity<>(new ResponseMessage("User registered successfully!"), HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/apiwatch/security/entities/ApiWatchUserDetails.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+package com.apiwatch.security.entities;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.apiwatch.entities.User;
+import com.apiwatch.security.service.UserDetailsServiceImpl;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * @author epa
+ *
+ */
+public class ApiWatchUserDetails implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	private static final Log log = LogFactory.getLog(ApiWatchUserDetails.class);
+	private String id;
+
+	private String username;
+
+	private String email;
+
+	@JsonIgnore
+	private String password;
+
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public ApiWatchUserDetails(String id, String username, String email, String password,
+			Collection<? extends GrantedAuthority> authorities) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.authorities = authorities;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public static ApiWatchUserDetails build(User user) {
+		log.info("user :"+user.toString());
+		List<GrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role))
+				.collect(Collectors.toList());
+		return new ApiWatchUserDetails(user.getId(), user.getUsername(), user.getEmail(),
+				user.getPassword(), authorities);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.security.core.userdetails.UserDetails#getAuthorities(
+	 * )
+	 */
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return authorities;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.security.core.userdetails.UserDetails#getPassword()
+	 */
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.security.core.userdetails.UserDetails#getUsername()
+	 */
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#
+	 * isAccountNonExpired()
+	 */
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#
+	 * isAccountNonLocked()
+	 */
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#
+	 * isCredentialsNonExpired()
+	 */
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.security.core.userdetails.UserDetails#isEnabled()
+	 */
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        ApiWatchUserDetails user = (ApiWatchUserDetails) o;
+        return Objects.equals(id, user.id);
+    }
+
+}
