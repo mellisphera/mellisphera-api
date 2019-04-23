ffb51f76c9e5b9ca7418517622857399889d5e61
Mickael <pezzoni.mickael@gmail.com>
2019-04-23 15:16:47 +0200

    modif requete obs

--- src/main/java/com/mellisphera/controllers/RecordController.java
+++ src/main/java/com/mellisphera/controllers/RecordController.java
@@ -72,7 +72,7 @@ public class RecordController {
         Date start  = range[0];
         Date end = range[1];
         List<Record> rec = null;
-        rec = this.recordRepository.findByIdHiveAndRecordDateBetween(idHive, start, end,sort);
+        rec = this.recordRepository.findByIdHiveAndRecordDateBetween(idHive, start,end, sort);
         if(rec != null) {
         	return new ResponseEntity<>(rec, HttpStatus.OK);
         }
