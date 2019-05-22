f4b1aae843824a803588fff7dfb038e9a4669d9c
Pezzoni <pezzoni.mickael@gmail.com>
2019-05-22 11:27:22 +0200

    ajout envoi mail & random password

--- src/main/java/com/mellisphera/controllers/AuthRestApiController.java
+++ src/main/java/com/mellisphera/controllers/AuthRestApiController.java
@@ -10,6 +10,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -21,6 +23,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +34,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mellisphera.entities.Connection;
 import com.mellisphera.entities.User;
 import com.mellisphera.entities.UserPref;
+import com.mellisphera.mail.PasswordGenerator;
 import com.mellisphera.repositories.ConnectionRepository;
 import com.mellisphera.repositories.UserRepository;
 import com.mellisphera.security.entities.ApiWatchUserDetails;
@@ -67,6 +75,9 @@ public class AuthRestApiController {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired PasswordGenerator passwordGenerator;
+	
+	
 	@Autowired
 	ConnectionRepository connectionRepository;
 
@@ -81,7 +92,8 @@ public class AuthRestApiController {
 
 	@Autowired
 	BmAuthServiceImpl bmAuthService;
-
+	
+	@Autowired JavaMailSender emailSender;
 	/**
 	 * 
 	 * @param loginRequest
@@ -186,5 +198,23 @@ public class AuthRestApiController {
 		userRepository.insert(user);
 		//
 		return new ResponseEntity<>(new ResponseMessage("User registered successfully!"), HttpStatus.OK);
+	}
+	
+	@PostMapping("/reset")
+	public void sendMail(@RequestBody String email)  throws MessagingException {
+		User user = this.userRepository.findUserByEmail(email);
+		String newPassword = this.passwordGenerator.getPassword();
+		user.setPassword(this.encoder.encode(newPassword));
+		this.userRepository.save(user);
+		MimeMessage msg = emailSender.createMimeMessage();
+		boolean multipart = true;
+		MimeMessageHelper helper = new MimeMessageHelper(msg, multipart, "utf-8");
+		String htmlMsg = "<p><img src='https://www.mellisphera.com/wp-content/uploads/2018/12/mellisphera-logo.png'/></p>"
+						+ "<h2>New password  : "+ newPassword +"</h2>";
+		msg.setContent(htmlMsg, "text/html");
+		helper.setTo(email);
+		helper.setSubject("Reset password");
+		this.emailSender.send(msg);
+		
 	}
 }
---
+++ src/main/java/com/mellisphera/mail/MailConfig.java
@@ -0,0 +1,37 @@
+package com.mellisphera.mail;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+	
+	 @Value("${mellisphera.app.mail.mail}")
+	 private String email;
+	 @Value("${mellisphera.app.mail.password}")
+	 private String password;
+	 @Value("${mellisphera.app.mail.host}")
+	 private String host;
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(host);
+		mailSender.setPort(587);
+		mailSender.setUsername(email);
+		mailSender.setPassword(password);
+		Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+		
+        return mailSender;
+	}
+
+}
