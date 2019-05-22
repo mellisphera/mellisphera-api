fd661e0ab84c713523b4dd55181deefae6d8e68c
Pezzoni <pezzoni.mickael@gmail.com>
2019-05-22 15:36:41 +0200

    modif daily

--- src/main/java/com/mellisphera/entities/DailyRecordsTH.java
+++ src/main/java/com/mellisphera/entities/DailyRecordsTH.java
@@ -19,16 +19,17 @@ public class DailyRecordsTH {
 	private String status;
 	private String trend;
 	private float r_int_text;
-        public Long timestamp;
-	
-	
+	public Long timestamp;
+	public int vitality;
+
+
 	public DailyRecordsTH(){
-		
+
 	}
-	
+
 	public DailyRecordsTH(String id, Date recordDate, String idHive, float humidity_int_min, float humidity_int_max,
 			float temp_int_min, float temp_int_max, float temp_int_moy, float temp_int_stddev, String status,
-			String trend, float r_int_text, Long timestamp) {
+			String trend, float r_int_text, Long timestamp, int vitality) {
 		super();
 		this.id = id;
 		this.recordDate = recordDate;
@@ -42,112 +43,137 @@ public class DailyRecordsTH {
 		this.status = status;
 		this.trend = trend;
 		this.r_int_text = r_int_text;
-                this.timestamp = timestamp;
+		this.timestamp = timestamp;
+		this.vitality = vitality;
 	}
-	
-	
+
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTrend() {
+		return trend;
+	}
+
+	public void setTrend(String trend) {
+		this.trend = trend;
+	}
+
+	public int getVitality() {
+		return vitality;
+	}
+
+	public void setVitality(int vitality) {
+		this.vitality = vitality;
+	}
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-        public Long getTimestamp(){
-            return this.timestamp;
-        }
+
+	public Long getTimestamp(){
+		return this.timestamp;
+	}
 	public Date getRecordDate() {
 		return recordDate;
 	}
-	
-        public void setTimestamp(Long newTimestamp){
-            this.timestamp = timestamp;
-        }
+
+	public void setTimestamp(Long newTimestamp){
+		this.timestamp = timestamp;
+	}
 	public void setRecordDate(Date recordDate) {
 		this.recordDate = recordDate;
 	}
-	
+
 	public String getIdHive() {
 		return idHive;
 	}
-	
+
 	public void setIdHive(String idHive) {
 		this.idHive = idHive;
 	}
-	
+
 	public float getHumidity_int_min() {
 		return humidity_int_min;
 	}
-	
+
 	public void setHumidity_int_min(float humidity_int_min) {
 		this.humidity_int_min = humidity_int_min;
 	}
-	
+
 	public float getHumidity_int_max() {
 		return humidity_int_max;
 	}
-	
+
 	public void setHumidity_int_max(float humidity_int_max) {
 		this.humidity_int_max = humidity_int_max;
 	}
-	
+
 	public float getTemp_int_min() {
 		return temp_int_min;
 	}
-	
+
 	public void setTemp_int_min(float temp_int_min) {
 		this.temp_int_min = temp_int_min;
 	}
-	
+
 	public float getTemp_int_max() {
 		return temp_int_max;
 	}
-	
+
 	public void setTemp_int_max(float temp_int_max) {
 		this.temp_int_max = temp_int_max;
 	}
-	
+
 	public float getTemp_int_moy() {
 		return temp_int_moy;
 	}
-	
+
 	public void setTemp_int_moy(float temp_int_moy) {
 		this.temp_int_moy = temp_int_moy;
 	}
-	
+
 	public float getTemp_int_stddev() {
 		return temp_int_stddev;
 	}
-	
+
 	public void setTemp_int_stddev(float temp_int_stddev) {
 		this.temp_int_stddev = temp_int_stddev;
 	}
-	
+
 	public String getHealth_status() {
 		return status;
 	}
-	
+
 	public void setHealth_status(String status) {
 		this.status = status;
 	}
-	
+
 	public String getHealth_trend() {
 		return trend;
 	}
-	
+
 	public void setHealth_trend(String trend) {
 		this.trend = trend;
 	}
-	
+
 	public float getR_int_text() {
 		return r_int_text;
 	}
-	
+
 	public void setR_int_text(float r_int_text) {
 		this.r_int_text = r_int_text;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "DailyRecordsTH [id=" + id + ", recordDate=" + recordDate + ", idHive=" + idHive + ", humidity_int_min="
@@ -156,5 +182,5 @@ public class DailyRecordsTH {
 				+ temp_int_stddev + ", status=" + status + ", trend=" + trend
 				+ ", r_int_text=" + r_int_text + "]";
 	}
-	
+
 }
