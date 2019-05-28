a033551eb5a7d66327c824b14a8259bf23781b7b
Mickael <pezzoni.mickael@gmail.com>
2019-05-28 16:50:22 +0200

    correction class sensor

--- src/main/java/com/mellisphera/controllers/SensorController.java
+++ src/main/java/com/mellisphera/controllers/SensorController.java
@@ -49,11 +49,12 @@ public class SensorController {
 		//this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
+	
 	@RequestMapping(value = "", method = RequestMethod.POST, produces={"application/json"})
 	public Sensor insert(@RequestBody Sensor sensor){
 		Hive hive = this.hivesRepository.findHiveById(sensor.getIdHive());
 		List<Sensor> sensorHive = this.sensorRepository.findSensorByIdHive(hive.getId());
-		hive.setSensor(!sensorHive.isEmpty());
+		hive.setSensor(!sensorHive.isEmpty()); 
 		this.hivesRepository.save(hive);
 		return this.sensorRepository.insert(sensor);
 	}
