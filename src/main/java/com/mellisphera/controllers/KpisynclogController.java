01de24dcb011a86346691b3b2cfd714ffae99046
pezzoni <pezzoni.mickael@gmail.com>
2019-06-05 16:18:31 +0200

    Kpisynclog ajout date requete

--- src/main/java/com/mellisphera/controllers/KpisynclogController.java
+++ src/main/java/com/mellisphera/controllers/KpisynclogController.java
@@ -1,10 +1,13 @@
 package com.mellisphera.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +16,21 @@ import com.mellisphera.repositories.KpisynclogRepository;
 
 @RestController
 @RequestMapping("/Kpisynclog")
-@PreAuthorize(" hasRole('ADMIN')") 
+@PreAuthorize("hasRole('ADMIN')") 
 public class KpisynclogController {
 	
 	@Autowired private KpisynclogRepository kpisynclogRepository;
 	public KpisynclogController() {}
 	
-	@GetMapping("")
+	@GetMapping("/all")
 	public List<Kpisynclog> getAll() {
 		return this.kpisynclogRepository.findAll();
+	}
+	
+	@PostMapping("/between")
+	public List<Kpisynclog> getByRange(@RequestBody Date start) {
+		return this.kpisynclogRepository.findByDateBetween(start, new Date());
+		
 	}
 	
 	
--- src/main/java/com/mellisphera/repositories/KpisynclogRepository.java
+++ src/main/java/com/mellisphera/repositories/KpisynclogRepository.java
@@ -1,9 +1,14 @@
 package com.mellisphera.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mellisphera.entities.Kpisynclog;
 
 public interface KpisynclogRepository extends MongoRepository<Kpisynclog, String> {
+	
+	public List<Kpisynclog> findByDateBetween(Date start, Date end);
 
 }
