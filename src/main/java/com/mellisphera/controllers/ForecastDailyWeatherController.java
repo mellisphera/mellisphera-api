05dfd2d3926bf1b269af53f75ac9b1529ea1dbc3
mickael2407 <pezzoni.mickael@gmail.com>
2019-07-04 15:51:42 +0200

    add forecast request

--- src/main/java/com/mellisphera/controllers/ForecastDailyWeatherController.java
+++ src/main/java/com/mellisphera/controllers/ForecastDailyWeatherController.java
@@ -1,12 +1,32 @@
 package com.mellisphera.controllers;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mellisphera.entities.ForecastDailyWeather;
+import com.mellisphera.repositories.ForecastDailyWeatherRepository;
 
 @RestController
 @PreAuthorize("hasRole('PREMIUM') or hasRole('STANDARD') or hasRole('ADMIN')")
 @RequestMapping("/forecastDailyWeather")
 public class ForecastDailyWeatherController {
 
+	@Autowired private ForecastDailyWeatherRepository forecastDailyWeatherRepository;
+	
+	public ForecastDailyWeatherController() {
+		
+	}
+	
+	@PostMapping("apiary/{idApiary}")
+	public List<ForecastDailyWeather> getCurrentDailyWeatherByApiary(@PathVariable String idApiary, @RequestBody Date[] range) {
+		return this.forecastDailyWeatherRepository.findByIdApiaryAndDateBetween(idApiary, range[0], range[1]);
+	}
 }
--- src/main/java/com/mellisphera/repositories/ForecastDailyWeatherRepository.java
+++ src/main/java/com/mellisphera/repositories/ForecastDailyWeatherRepository.java
@@ -1,5 +1,13 @@
 package com.mellisphera.repositories;
 
+import java.util.Date;
+import java.util.List;
+
+import com.mellisphera.entities.ForecastDailyWeather;
+
+
 public interface ForecastDailyWeatherRepository {
+
+	public List<ForecastDailyWeather> findByIdApiaryAndDateBetween(String idApiary, Date start, Date end);
 
 }
