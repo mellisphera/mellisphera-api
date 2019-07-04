7d198d7bd09f26528eb2aadfbffb86b565414404
mickael2407 <pezzoni.mickael@gmail.com>
2019-07-04 15:56:31 +0200

    fix error ForecastDailyWeatherRepository

--- src/main/java/com/mellisphera/repositories/ForecastDailyWeatherRepository.java
+++ src/main/java/com/mellisphera/repositories/ForecastDailyWeatherRepository.java
@@ -3,11 +3,12 @@ package com.mellisphera.repositories;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.mellisphera.entities.ForecastDailyWeather;
 
 
-public interface ForecastDailyWeatherRepository {
-
+public interface ForecastDailyWeatherRepository extends MongoRepository<ForecastDailyWeather ,String> {
 	public List<ForecastDailyWeather> findByIdApiaryAndDateBetween(String idApiary, Date start, Date end);
 
 }
