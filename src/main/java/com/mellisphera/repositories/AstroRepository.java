73436d6192495347aa1b2285ff7c311c60954d31
mickael2407 <pezzoni.mickael@gmail.com>
2019-07-11 13:51:47 +0200

    fix astro repository

--- src/main/java/com/mellisphera/repositories/AstroRepository.java
+++ src/main/java/com/mellisphera/repositories/AstroRepository.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.mellisphera.entities.Astro;
 
+@Service
+@Repository
 public interface AstroRepository extends MongoRepository<Astro, String> {
 
 	public List<Astro> findByIdApiaryAndDateBetween(String idApiary, Date start, Date end);
