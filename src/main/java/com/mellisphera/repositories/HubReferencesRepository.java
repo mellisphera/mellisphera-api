df41f4f8f0b1d2afd066cba4ff62d8becb909e86
mickael2407 <pezzoni.mickael@gmail.com>
2020-02-03 15:31:44 +0100

    add hub request

--- src/main/java/com/mellisphera/repositories/HubReferencesRepository.java
+++ src/main/java/com/mellisphera/repositories/HubReferencesRepository.java
@@ -3,5 +3,9 @@ package com.mellisphera.repositories;
 import com.mellisphera.entities.HubReferences;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface HubReferencesRepository extends MongoRepository<HubReferences, String> {
+
+    public List<HubReferences> findByUserId(String userId);
 }
