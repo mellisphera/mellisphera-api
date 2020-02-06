db4198f08c02f2c6d40258e60b1dcbba3ac52f44
mickael2407 <pezzoni.mickael@gmail.com>
2020-02-06 15:09:49 +0100

    fix convertion

--- src/main/java/com/mellisphera/service/UnitService.java
+++ src/main/java/com/mellisphera/service/UnitService.java
@@ -2,39 +2,47 @@ package com.mellisphera.service;
 
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+
 @Service
 public class UnitService {
 
 
-    public double convertTempFromUsePref(double temp, Unit unit) {
+    DecimalFormat decimalFormat;
+
+    UnitService() {
+        this.decimalFormat = new DecimalFormat("0.#");
+    }
+
+    public String convertTempFromUsePref(double temp, Unit unit) {
         if (unit.equals(Unit.METRIC)) {
-            return (double)(Math.round(temp * 10) / 10);
+            return decimalFormat.format(temp);
         } else {
-            return (double)((Math.round(temp * 10) / 10) * 9 / 5 + 32);
+            return decimalFormat.format(temp * 9 / 5 + 32);
         }
     }
 
-    public double convertWeightFromUserPref(double weight, Unit unit) {
+    public String convertWeightFromUserPref(double weight, Unit unit) {
         if (unit.equals(Unit.METRIC)) {
-            return (double)(Math.round(weight * 10) / 10);
+            return decimalFormat.format(weight);
         } else {
-            return (double)((Math.round(weight * 10) / 10) * 2.2046);
+            return decimalFormat.format(weight * 2.2046);
         }
     }
 
-    public double convertRain(double rain, Unit unit) {
+    public String convertRain(double rain, Unit unit) {
         if (unit.equals(Unit.METRIC)) {
-            return (double)(Math.round(rain * 10) / 10);
+            return decimalFormat.format(rain);
         } else {
-            return (double)((Math.round(rain * 10) / 10) * 25.4);
+            return decimalFormat.format(rain * 25.4);
         }
     }
 
-    public double convertWind(double wind, Unit unit) {
+    public String convertWind(double wind, Unit unit) {
         if (unit.equals(Unit.METRIC)) {
-            return (double)(Math.round(wind * 10) / 10) * 3.6;
+            return decimalFormat.format(wind * 3.6);
         } else {
-            return (Math.round(wind * 10) / 10) * 2.276;
+            return decimalFormat.format(wind * 2.276);
         }
     }
 
