329f2584f84c55b240d525b897dc8b004bf3ed82
mickael2407 <pezzoni.mickael@gmail.com>
2021-05-07 14:53:02 +0200

    Fitness in explore

--- src/main/java/com/mellisphera/repositories/FitnessRepository.java
+++ src/main/java/com/mellisphera/repositories/FitnessRepository.java
@@ -2,6 +2,7 @@ package com.mellisphera.repositories;
 
 import com.mellisphera.entities.Fitness;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.domain.Sort;
 
 import java.util.Date;
 import java.util.List;
@@ -10,4 +11,5 @@ public interface FitnessRepository extends MongoRepository<Fitness, String> {
 
     List<Fitness> findByUserIdAndDateBetween(String userId, Date start, Date end);
     List<Fitness> findByUserId(String userId);
+    List<Fitness> findByHiveIdAndDateBetween(String userId, Date start, Date end, Sort sort);
 }
