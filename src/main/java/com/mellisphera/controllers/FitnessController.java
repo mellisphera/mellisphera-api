cec3bf156fa798bc6d2a95ec166111f28caad677
mickael2407 <pezzoni.mickael@gmail.com>
2021-05-07 14:53:02 +0200

    Fitness in explore

--- src/main/java/com/mellisphera/controllers/FitnessController.java
+++ src/main/java/com/mellisphera/controllers/FitnessController.java
@@ -9,9 +9,14 @@ import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import java.util.Date;
 import java.util.List;
@@ -36,5 +41,11 @@ public class FitnessController {
         );
         AggregationResults<Fitness> aggregationResults = this.mongoTemplate.aggregate(aggregation, "Fitness", Fitness.class);
         return aggregationResults.getMappedResults();
+    }
+
+    @PostMapping(value = "/daily/{hiveId}")
+    public List<Fitness> getFitnessByHiveIdAndDateBetween(@PathVariable String hiveId, @RequestBody Date[] range){
+        Sort sort = new Sort(Direction.DESC, "timestamp");
+        return this.fitnessRepository.findByHiveIdAndDateBetween(hiveId, range[0], range[1], sort);
     }
 }
